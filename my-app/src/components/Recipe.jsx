21893ab0422f2a60a16e3913a1d0b5f9de873061
rohitsoni78 <142527065+rohitsoni78@users.noreply.github.com>
2025-12-02 11:22:54 +0000

    myapp

--- my-app/src/components/Recipe.jsx
+++ my-app/src/components/Recipe.jsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 export default function Recipe()
+
 {
 
     const [recipename,setRecipeName]=useState("");
@@ -39,18 +41,18 @@ export default function Recipe()
         <>
         <div style={{maxWidth:"600px",margin:"20px auto",border:"1px solid #ccc",padding:"30px"}}>
             <h1 style={{marginBottom:"20px",border:"1px solid #ccc",padding:"20px"}}>Recipe Book</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
             <div style={{marginBottom:"10px"}}>
                 <label>Name:</label> <br />
-                <input type="text" style={{width:"100%",padding:"5px"}} />
+                <input type="text" style={{width:"100%",padding:"5px"}} value={recipename} onChange={(e)=>setRecipeName(e.target.value)}/>
             </div>
             <div style={{marginBottom:"10px"}}>
                 <label>Ingredients:</label> <br />
-                <input type="text" style={{width:"100%",padding:"5px"}} />
+                <input type="text" style={{width:"100%",padding:"5px"}}value={ingredients} onChange={(e)=>setIngredients(e.target.value)}/>
             </div>
             <div style={{marginBottom:"10px"}}>
                 <label>Instructions:</label> <br />
-                <input type="text" style={{width:"100%",padding:"5px"}} />
+                <input type="text" style={{width:"100%",padding:"5px"}}  value={instructions} onChange={(e)=>setInstructions(e.target.value)}/>
             </div>
 
             <button style={{padding:"5px 10px"}} type="submit">
